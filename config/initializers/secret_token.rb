dd17d11422deb002c45aec48f9e7007282e65976
Charles Peterson <nowled@operamail.com>
2012-02-18 21:12:29 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeachersJapan::Application.config.secret_token = '3610131f919e953dae50f3ad894f0e89a712ff7f2f293ceaedbd43aec2dd1f99f820f35f91cb4adc63f96bdcccc8a609132730aaa75eb7d6848c81d6e365a7d0'
